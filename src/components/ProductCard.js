a54803bcd90298dcb53e559a3c24057ca817365f
Amund-Ring <60844467+Amund-Ring@users.noreply.github.com>
2021-10-26 16:12:28 +0200

    Components added

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,12 +1,22 @@
-import React from 'react'
-import * as styles from '../styles/ProductCard.module.css'
+import { navigate } from "gatsby";
+import React from "react";
+import * as styles from "../styles/ProductCard.module.css";
 
-function ProductCard() {
+function ProductCard({ id }) {
+  const handleClick = () => {
+    console.log("Card clicked.");
+    navigate(`/product?id=${id}`);
+  };
+
   return (
-    <div className={styles.productCard}>
-      ProductCard
+    <div className={styles.productCard} onClick={handleClick}>
+      <img src={"/1.jpg"} alt="Alternative text" />
+      <div>
+        <h2>Philips hue bulb - Philips</h2>
+        <h3>500,-</h3>
+      </div>
     </div>
-  )
+  );
 }
 
-export default ProductCard
+export default ProductCard;
--- src/pages/products.js
+++ src/pages/products.js
@@ -1,13 +1,17 @@
 import React from "react";
 import Layout from "../components/Layout";
 import ProductCard from "../components/ProductCard";
-import * as styles from '../styles/products.module.css'
+import * as styles from "../styles/products.module.css";
 
 function products() {
   return (
     <Layout>
       <div className={styles.products}>
-        
+        <ProductCard id={1}/>
+        <ProductCard id={2}/>
+        <ProductCard id={3}/>
+        <ProductCard id={4}/>
+        <ProductCard id={5}/>
       </div>
     </Layout>
   );
