12636177a41f542643987c960734a9e05bd54317
Amund-Ring <60844467+Amund-Ring@users.noreply.github.com>
2021-10-27 21:25:04 +0200

    Product

--- src/pages/product.js
+++ src/pages/product.js
@@ -1,36 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useQueryParam, NumberParam } from "use-query-params";
 import { navigate } from "gatsby";
 import Layout from "../components/Layout";
 import ProductDetails from "../components/ProductDetails";
 import productData from "../data/products.json";
 
-
-
 function Product() {
-  // const [productId] = useQueryParam("id", NumberParam);
 
-  // if (typeof productId === "undefined" || isNaN(productId)) {
-  //   navigate("/404");
-  // }
 
-  // const productItem = productData.items.find(item => item.id === productId);
+  const [productId, setProductId] = useState(2);
+  // const [productId, setProductId] = useQueryParam("id", NumberParam);
 
-  // if (typeof productItem === "undefined") {
-  //   navigate("/404");
-  // }
 
-  // const goBack = () => {
-  //   navigate("/products");
-  // }
-  
+  if (typeof productId === "undefined" || isNaN(productId)) {
+    navigate("/404");
+  }
+
+  const productItem = productData.items.find(item => item.id === productId);
+
+  if (typeof productItem === "undefined") {
+    navigate("/404");
+  }
+
+  const goBack = () => {
+    navigate("/products");
+  };
 
   return (
     <Layout>
       <div className="item">
-        {/* <h3 onClick={goBack} >{`<-- Back`}</h3> eslint-disable-line */}
-        <h3 >{`<-- Back`}</h3> {/* eslint-disable-line */}
-        {/* <ProductDetails item={productItem} /> */}
+        <h3 onClick={goBack}>{`<-- Back`}</h3> {/* eslint-disable-line */}
+        <ProductDetails item={productItem} />
       </div>
     </Layout>
   );
